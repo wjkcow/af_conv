5ed49971206fb83a7e99030a6c006010d4f0a103
wjkcow <wjkcow@deeplearn2.eecs.umich.edu>
2014-09-19 21:26:47 -0400

    working on af_conv function

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CTAGS = -m64 -Wall -Werror -pthread -O2 -DNDEBUG -DAFCL -lrt -Wl,--no-as-needed
 INCLUDEFLAGS =  -I/usr/local/arrayfire/include
 LINKFLAGS = -L/usr/local/arrayfire/lib64 -lpthread -lstdc++ -lm -Wl,-rpath,/usr/local/arrayfire/lib64,-rpath,/usr/local/arrayfire/lib64 -lafcl
-af_conv: 
+af_conv: af_conv.cpp
 	cc $(CTAGS) $(INCLUDEFLAGS) $(LINKFLAGS)  af_conv.cpp -o af_conv
 
 clean:
--- af_conv.cpp
+++ af_conv.cpp
@@ -4,8 +4,10 @@
 #include <af/utils.h>
 
 using namespace af;
-
-
+int main(){
+	af::deviceset(0);
+	af::info();
+}
 int af_conv(float *img, float * kernel, int img_x, int img_y, int img_s,
     int ker_x, int ker_y, int ker_s, int ch, float *result){
 
@@ -13,14 +15,17 @@ int af_conv(float *img, float * kernel, int img_x, int img_y, int img_s,
         int device = 0; // free version only support 0
         af::deviceset(device);
         af::info();
-        array img(img_x, img_y, ch, img_s, img, afHost);
-        array ker(ker_x, ker_y, ch, ker_s, kernel, afHost);
-        array result = constant(0, img_x - ker_x + 1, img_y - ker_y + 1, ker_s, img_s);
+        array Img(img_x, img_y, ch, img_s, img, afHost);
+        array Ker(ker_x, ker_y, ch, ker_s, kernel, afHost);
+        array Result = constant(0, img_x - ker_x + 1, img_y - ker_y + 1, ker_s, img_s);
 
         gfor(array i, img_s * ker_s){
             for(int c = 0; c < ch; ++c){
-                result(span, span, i / ker_s, i / img_s) += convolve(img(span, span, c, i / ker_s), ker(span, span, c, i % img_s))(seq(img_x - ker_x + 1),
-                    seq(img_y - ker_y + 1));
+		int img_stack_n = i.elements() / ker_s;
+		int ker_stack_n = i.elements() % img_s;
+		array c_pic = convolve(Img(span, span, c, img_stack_n), Ker(span, span, c, ker_stack_n));
+                Result(span, span, ker_stack_n, img_stack_n) += c_pic(seq(img_x - ker_x + 1),
+					seq(img_y - ker_y + 1));
             }
         }
 
@@ -28,5 +33,6 @@ int af_conv(float *img, float * kernel, int img_x, int img_y, int img_s,
         fprintf(stderr, "%s\n", e.what());
         throw;
     }
+	return 0;
 
 }
