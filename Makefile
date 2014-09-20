81410952770f13f438ead8a9a1dd3ee3506a099c
Jingkui Wang <wjkcow@gmail.com>
2014-09-20 02:28:19 -0400

    add make

--- Makefile
+++ Makefile
@@ -1,6 +1,21 @@
 CTAGS = -m64 -Wall -Werror -pthread -O2 -DNDEBUG -DAFCL -lrt -Wl,--no-as-needed
 INCLUDEFLAGS =  -I/usr/local/arrayfire/include
 LINKFLAGS = -L/usr/local/arrayfire/lib64 -lpthread -lstdc++ -lm -Wl,-rpath,/usr/local/arrayfire/lib64,-rpath,/usr/local/arrayfire/lib64 -lafcl
+AR = ar
+MEX = /usr/local/MATLAB/R2013b/bin/mex
+
+
+
+mex:     libaf_conv.a
+	${MEX} -L. -laf_conv -v af_conv_valid.cpp LINKFLAGS
+
+
+libaf_conv.a: af_conv.cpp
+	cc $(CTAGS) $(INCLUDEFLAGS) $(LINKFLAGS)  af_conv.cpp -o af_conv.o
+	${AR} -r libaf_conv.a af_conv.o
+
+
+
 af_conv: af_conv.cpp
 	cc $(CTAGS) $(INCLUDEFLAGS) $(LINKFLAGS)  af_conv.cpp -o af_conv
 
--- af_conv.cpp
+++ af_conv.cpp
@@ -4,10 +4,19 @@
 #include <af/utils.h>
 
 using namespace af;
-int main(){
-	af::deviceset(0);
-	af::info();
-}
+// int main(){
+// 	af::deviceset(0);
+// 	af::info();
+
+
+//     img.image_base = new float[50];
+//     ker.kernel_base = new float[2];
+
+//     memset(img.image_base, 0, 50);
+//     memset(ker.kernel_base, 0, 2);
+//     float * result = new float[1000];
+
+// }
 int af_conv(float *img, float * kernel, int img_x, int img_y, int img_s,
     int ker_x, int ker_y, int ker_s, int ch, float *result){
 
@@ -21,13 +30,14 @@ int af_conv(float *img, float * kernel, int img_x, int img_y, int img_s,
 
         gfor(array i, img_s * ker_s){
             for(int c = 0; c < ch; ++c){
-		int img_stack_n = i.elements() / ker_s;
-		int ker_stack_n = i.elements() % img_s;
-		array c_pic = convolve(Img(span, span, c, img_stack_n), Ker(span, span, c, ker_stack_n));
-                Result(span, span, ker_stack_n, img_stack_n) += c_pic(seq(img_x - ker_x + 1),
+              int img_stack_n = i.elements() / ker_s;
+              int ker_stack_n = i.elements() % img_s;
+              array c_pic = convolve(Img(span, span, c, img_stack_n), Ker(span, span, c, ker_stack_n));
+              Result(span, span, ker_stack_n, img_stack_n) += c_pic(seq(img_x - ker_x + 1),
 					seq(img_y - ker_y + 1));
             }
         }
+        Result.host<float>(result);
 
     } catch (af::exception& e) {
         fprintf(stderr, "%s\n", e.what());
